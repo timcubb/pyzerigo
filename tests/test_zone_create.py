bd2e886d3f9b4d15dbcc57cdc23d2ac4ec419c9c
lopter <none@none>
2010-01-22 14:20:38 +0100

    Clarify the test on zone creation

--- tests/test_zone_create.py
+++ tests/test_zone_create.py
@@ -2,9 +2,17 @@
 #
 # -*- coding: utf-8 -*-
 
+import sys
+
 import zerigo
 
 if __name__ == '__main__':
     zone = zerigo.Zone('test.vmlayers.org')
     zone.create()
-    zone.create()
+    try:
+        zone.create()
+    except zerigo.CreateError:
+        pass
+    else:
+        print 'Fail: double create of a zone is impossible'
+        sys.exit(1)
